7f4ec71119f127bfe13cd7bfb2a407192e7cbdb4
Eyosiyas Girma <eyosiyasgirma0@gmail.com>
2019-11-10 16:17:13 -0800

    filtering modified

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-function usernameSearch(username) {
-  console.log("username search" + username);
-}
-function locationSearch(location) {
-  console.log("location search" + location);
+function searchBoth(username,lists) {
+  let usernameslist = lists;
+
+  for (var usernames of usernameslist) {
+    let content = usernames.innerHTML.toLowerCase();
+    if (username == "") {
+      let card =
+        usernames.parentElement.parentElement.parentElement.parentElement;
+      
+      card.style.display = "";
+    } else if (!content.includes(username)) {
+      let card =
+        usernames.parentElement.parentElement.parentElement.parentElement;
+      card.style.display = "none";
+    }else{
+      let card =
+        usernames.parentElement.parentElement.parentElement.parentElement;
+      card.style.display = "";
+    }
+  }
 }
 
 function search(selectedValue, input) {
+  let list;
   switch (selectedValue) {
     case "1":
-      usernameSearch(input);
+      list = document.querySelectorAll(".uname");
+      searchBoth(input,list);
       break;
     case "2":
-      locationSearch(input);
+      list = document.querySelectorAll(".country");
+      searchBoth(input,list);
       break;
   }
 }
