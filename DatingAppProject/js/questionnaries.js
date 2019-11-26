46720f927dda7b62c4304fac897169ddf7e2a272
betse1332 <48962290+betse1332@users.noreply.github.com>
2019-11-26 20:58:07 +0300

    Add files via upload

---
+++ DatingAppProject/js/questionnaries.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // $(".modal-trigger").leanModal();
+    // $("#modal1").modal();
+    $(".modal").openModal();
+    $(".choice a").click(function(){
+        $(this).addClass("teal");
+    });
+   
+    // $(".modalC").click(function(){
+    //     $(".modal").closeModal();
+    // });
+    // alert("hello there");
+})
